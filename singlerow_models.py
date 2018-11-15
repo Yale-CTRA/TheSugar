4a43b9b2f85fadcc648dc6a294e43f719fdac5d5
Aditya Biswas <38960177+aditya-biswas@users.noreply.github.com>
2018-11-15 17:38:54 -0500

    Add files via upload

--- singlerow_models.py
+++ singlerow_models.py
@@ -30,34 +30,44 @@ diabetes, liver_disease, ckd".split(sep = ', ')
 outcome = 'hypo40'
 
 ############################################################################
-
+## xgboost model
 
 import xgboost as xgb
 from xgboost import plot_tree
+from sklearn.metrics import roc_auc_score as AUC
 import graphviz
 
 cutoff = int(np.round(len(data)*0.7))
-train, test = data.iloc[:cutoff,:], data.iloc[cutoff:,:]
+iters = 100
+aucs = np.zeros(iters, dtype = np.float32)
+for i in range(iters):
+    data = data.sample(frac = 1.)
+    train, test = data.iloc[:cutoff,:], data.iloc[cutoff:,:]
+    
+    dtrain = xgb.DMatrix(train[predictors], label = train[outcome])
+    dtest = xgb.DMatrix(test[predictors], label = test[outcome])
+    
+    param = {'max_depth': 6, 'eta': 0.05, 'silent': 1, 'objective': 'binary:logistic'}
+    #param['eval_metric'] = 'auc'
+    #evallist = [ (dtrain, 'train'), (dtest, 'eval') ]
+    num_round = 150
+    
+    bst = xgb.train(param, dtrain, num_round)#, evallist)
+    Ptest = bst.predict(dtest)
+    aucs[i] = AUC(test[outcome].values, Ptest)
+    if (i+1) % 10 == 0:
+        print(i+1, ' iterations completed')
+    #bst.get_score()
 
-dtrain = xgb.DMatrix(train[predictors], label = train[outcome])
-dtest = xgb.DMatrix(test[predictors], label = test[outcome])
-
-param = {'max_depth': 6, 'eta': 0.05, 'silent': 1, 'objective': 'binary:logistic'}
-param['eval_metric'] = 'auc'
-evallist = [ (dtrain, 'train'), (dtest, 'eval') ]
-num_round = 150
-
-bst = xgb.train(param, dtrain, num_round, evallist)
-Ptrain_xgb = bst.predict(dtrain)
-
-bst.get_score()
+# mean: 0.775, std: 0.013
 
 ##################################################################################
+## logistic regression model
 
 from sklearn.linear_model import LogisticRegression as LR
 from sklearn.metrics import roc_auc_score as AUC
 from Helper.utilities import showCoef
-from Helper.preprocessing import Imputer
+from Helper.preprocessing import Transformer
 
 cutoff = int(np.round(len(data)*0.7))
 iters = 100
@@ -71,13 +81,13 @@ for i in range(iters):
     Ytrain = train[outcome].values
     Ytest = test[outcome].values
     
-    imputer = Imputer()
-    Xtrain = imputer.fit_transform(Xtrain)
-    Xtest = imputer.transform(Xtest)
+    transformer = Transformer()
+    Xtrain = transformer.fit_transform(Xtrain)
+    Xtest = transformer.transform(Xtest)
     
     
     model = LR(class_weight = 'balanced', C = 1e-1)
-    model.fit(Xtrain, Ytrain)#, sample_weight = weights)
+    model.fit(Xtrain, Ytrain)
     #showCoef(model.coef_[0], predictors)
     
     #Ptrain = model.predict_proba(Xtrain)[:,1]
@@ -86,4 +96,5 @@ for i in range(iters):
     aucs[i] = AUC(Ytest, Ptest)
     if (i+1) % 10 == 0:
         print(i+1, ' iterations completed')
-    
+
+## mean: 0.755, std = 0.013
